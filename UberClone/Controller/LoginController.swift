87f36f3cb32916ee98d248a5e9003a84d63b42b6
cesaroer <cesarvarcard@gmail.com>
2021-09-08 23:58:58 -0500

    Added Uber Title and background Color to black

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    //MARK: - Properties
 
+    private let titleLabel : UILabel = {
+       
+        let label = UILabel()
+        label.text = "Uber"
+        label.font = UIFont(name: "Avenir", size: 50.0)
+        label.textColor = .white
+        return label
+    }()
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        view.backgroundColor = .red
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
